173a80ab21371476602454c669406b4d8db2c186
binaya <ayanibamrahs@gmail.com>
2019-10-03 19:24:46 +0545

    image carousel oop method

---
+++ js-assignments/image-carousel-try2/carousel.js
@@ -0,0 +1,259 @@
+
+function slider(imageSourceList = [], animationSpeed = 60, holdTime = 3){
+
+    this.container;
+    this.wrapper;
+    this.leftButton;
+    this.rightButton;
+    this.indicatorDiv;
+    this.currentSlide = 0;
+    this.currentIndicator;
+    this.currentSliderLeftPosition = 0;
+    this.nextSliderLeftPosition = 0;
+    this.containerWidth;
+    this.containerHeight;
+    this.numberOfImages;
+    this.MAXIMUM_LEFT_POSITION = 0;
+    this.MINIMUM_LEFT_POSITION;
+
+    this.init = function(){
+        this.addContainer();
+        this.addWrapper();
+        this.addImages();
+        this.addLeftButton();
+        this.addRightButton();
+        this.addIndicators();
+
+        this.leftButton.onclick = leftSlide.bind(this);
+        this.rightButton.onclick = rightSlide.bind(this);
+        
+    }
+
+
+    this.addContainer = function(){
+        this.container = document.createElement('div');
+        this.container.classList.add('carousel-container');
+
+        document.body.appendChild(this.container);
+
+        var containerStyle = getComputedStyle(this.container);
+        this.containerWidth = parseInt(containerStyle.width)
+        this.containerHeight = parseInt(containerStyle.height);
+    }
+
+    this.addWrapper = function(){
+        this.wrapper = document.createElement('div');
+        this.wrapper.style.position = 'absolute';
+        this.wrapper.style.left = '0px';
+        this.wrapper.classList.add('carousel-image-wrapper');
+
+        var totalSliderWidth;
+        this.numberOfImages = imageSourceList.length;
+        totalSliderWidth = this.containerWidth * this.numberOfImages;
+        
+        this.wrapper.style.height = this.containerHeight;
+        this.wrapper.style.width = totalSliderWidth + 'px';
+        this.wrapper.style.zIndex = '10';
+
+        this.container.appendChild(this.wrapper)
+
+        this.MINIMUM_LEFT_POSITION = - (this.numberOfImages) * this.containerWidth;
+
+        // console.log(totalSliderWidth);
+        // console.log(imageSourceList.length);
+    }
+
+    this.addImages = function(){
+
+        for(var i = 0; i < this.numberOfImages; i++){
+            var img = document.createElement('img');
+            img.setAttribute('src', imageSourceList[i]);
+            img.style.width = this.containerWidth;
+            img.style.height = this.containerHeight;
+            img.style.cssFloat = 'left';
+            img.style.objectFit = 'cover';
+            this.wrapper.appendChild(img);
+
+        }
+    }
+
+    this.addLeftButton = function(){
+        this.leftButton = document.createElement('button');
+        this.leftButton.style.width = '40px';
+        this.leftButton.style.height = '40px';
+        this.leftButton.style.borderRadius = '50%';
+        this.leftButton.style.backgroundColor = 'lightgray';
+        this.leftButton.style.zIndex = '20';
+        this.leftButton.style.position = 'absolute';
+        this.leftButton.style.left = '2px';
+        this.leftButton.style.border = 'none';
+        this.leftButton.style.top = (this.containerHeight / 2 -
+                                parseInt(this.leftButton.style.height)) + 'px';
+
+        var leftButtonPara = document.createElement('para');
+        leftButtonPara.innerHTML = '&#8249;';
+        this.leftButton.appendChild(leftButtonPara);
+        this.container.appendChild(this.leftButton);
+    }
+
+    this.addRightButton = function(){
+        this.rightButton = document.createElement('button');
+        this.rightButton.style.width = '40px';
+        this.rightButton.style.height = '40px';
+        this.rightButton.style.borderRadius = '50%';
+        this.rightButton.style.backgroundColor = 'lightgray';
+        this.rightButton.style.zIndex = '20';
+        this.rightButton.style.position = 'absolute'
+        this.rightButton.style.right = '2px'
+        this.rightButton.style.border = 'none';
+        this.rightButton.style.top = (this.containerHeight / 2 -
+                                parseInt(this.rightButton.style.height)) + 'px';
+
+        var rightButtonPara = document.createElement('para');
+        rightButtonPara.innerHTML = '&#8250;';
+        this.rightButton.appendChild(rightButtonPara);
+        this.container.appendChild(this.rightButton);
+    }   
+
+    this.addIndicators = function(){
+
+        this.indicatorDiv = document.createElement('div');
+
+        this.indicatorDiv.classList.add('indicator-div')
+
+        this.indicatorDiv.style.position = 'absolute';
+        this.indicatorDiv.style.bottom = '5px';
+        this.indicatorDiv.style.zIndex = '20';
+        this.indicatorDiv.style.width = '100%';
+        this.indicatorDiv.style.textAlign = 'center';
+
+        this.activeIndicator = getIndividualIndicator();
+        this.activeIndicator.style.backgroundColor = 'rgba(255,255,255,1)';
+        this.activeIndicator.style.width = '18px';
+        this.activeIndicator.style.height = '18px';
+        
+        this.indicatorDiv.appendChild(this.activeIndicator);
+
+        for(var i = 1; i<this.numberOfImages; i++){
+            this.indicatorDiv.appendChild(getIndividualIndicator());
+        }
+        this.container.appendChild(this.indicatorDiv);
+
+        
+        
+        function getIndividualIndicator(){
+
+            var indicator = document.createElement('button');
+            indicator.style.width = '15px';
+            indicator.style.height = '15px';
+            indicator.style.borderRadius = '50%';
+            indicator.style.backgroundColor = 'rgba(255,255,255,0.5)';
+            indicator.style.border = 'none';
+            indicator.style.marginRight= '10px';
+            return indicator;
+        }
+
+    }
+
+    
+    function leftSlide(e){
+        
+        this.nextSliderLeftPosition = this.nextSliderLeftPosition - this.containerWidth;
+    
+        if (this.nextSliderLeftPosition <= this.MINIMUM_LEFT_POSITION){
+            this.nextSliderLeftPosition = 0;
+
+            var id1 = setInterval(frame1.bind(this), 1000/animationSpeed);
+            
+            function frame1(){
+            
+                if(this.currentSliderLeftPosition >= this.nextSliderLeftPosition){
+                    this.currentSliderLeftPosition = this.nextSliderLeftPosition;
+                    this.wrapper.style.left = this.nextSliderLeftPosition + 'px';                        
+                    clearInterval(id1);
+                }
+                else{
+                    this.currentSliderLeftPosition = this.currentSliderLeftPosition + 100;
+                    this.wrapper.style.left = this.currentSliderLeftPosition + 'px';
+                
+                }
+            }
+        }
+
+        else{
+            //console.log('nextPos: ' + this.nextSliderLeftPosition + ' , currentPos:' + this.currentSliderLeftPosition);                
+        
+            var id = setInterval(frame.bind(this), 1000/animationSpeed );
+            
+            function frame(){
+            
+                if(this.currentSliderLeftPosition <= this.nextSliderLeftPosition){
+                    this.currentSliderLeftPosition = this.nextSliderLeftPosition;
+                    this.wrapper.style.left = this.nextSliderLeftPosition + 'px';
+                    clearInterval(id);
+                }
+                else{
+                    this.currentSliderLeftPosition = this.currentSliderLeftPosition - 10;
+                    this.wrapper.style.left = this.currentSliderLeftPosition + 'px';
+                    
+                }
+            }
+        }
+    }
+
+    function rightSlide(e){
+        
+        this.nextSliderLeftPosition = this.nextSliderLeftPosition + this.containerWidth;
+        //console.log(this.MINIMUM_LEFT_POSITION);
+    
+        if (this.nextSliderLeftPosition > this.MAXIMUM_LEFT_POSITION){
+            this.nextSliderLeftPosition = this.MINIMUM_LEFT_POSITION + this.containerWidth;
+
+            var id1 = setInterval(frame1.bind(this), 1000/animationSpeed);
+            
+            function frame1(){
+            
+                if(this.currentSliderLeftPosition <= this.nextSliderLeftPosition){
+                    this.currentSliderLeftPosition = this.nextSliderLeftPosition;
+                    this.wrapper.style.left = this.nextSliderLeftPosition + 'px';                        
+                    clearInterval(id1);
+                }
+                else{
+                    this.currentSliderLeftPosition = this.currentSliderLeftPosition - 100;
+                    this.wrapper.style.left = this.currentSliderLeftPosition + 'px';
+                
+                }
+            }
+        }
+
+        else{
+            //console.log('nextPos: ' + this.nextSliderLeftPosition + ' , currentPos:' + this.currentSliderLeftPosition);                
+        
+            var id = setInterval(frame.bind(this), 1000/animationSpeed );
+            
+            function frame(){
+            
+                if(this.currentSliderLeftPosition >= this.nextSliderLeftPosition){
+                    this.currentSliderLeftPosition = this.nextSliderLeftPosition;
+                    this.wrapper.style.left = this.nextSliderLeftPosition + 'px';
+                    clearInterval(id);
+                }
+                else{
+                    this.currentSliderLeftPosition = this.currentSliderLeftPosition + 10;
+                    this.wrapper.style.left = this.currentSliderLeftPosition + 'px';
+                    
+                }
+            }
+        }
+    }
+}
+
+
+var imgList = ["images/image1.jpg","images/image2.jpg",
+"images/image3.jpg","images/image4.jpg",
+"images/image5.jpg"]
+
+var s = new slider(imgList,30).init();
+var d = new slider(imgList,60).init();
+var e = new slider(imgList,90).init();
+var f = new slider(imgList,120).init();
--- js-assignments/image-carousel/script.js
+++ js-assignments/image-carousel/script.js
@@ -1,191 +1,212 @@
-var carouselContainer;
-var carouselContainerStyle;
-var carouselContainerWidth;
-var carouselContainerHeight;
-var carouselImageWrapper;
-var carouselImageWrapperStyle;
-var carouselImagesList;
-var totalSliderWidth;
-var wrapperLeft;
-var leftButton;
-var rightButton;
-var MAX_LEFT;
-var activeIndicator;
-var imageWidth;
+
+var sl = new slider(0);
 
 
-init();
+function slider(n){
+
+    var carouselContainer;
+    var carouselContainerStyle;
+    var carouselContainerWidth;
+    var carouselContainerHeight;
+    var carouselImageWrapper;
+    var carouselImageWrapperStyle;
+    var carouselImagesList;
+    var totalSliderWidth;
+    var wrapperLeft;
+    var leftButton;
+    var rightButton;
+    var MAX_LEFT;
+    var activeIndicator;
+    var imageWidth;
+    var indicatorDiv;
+    var totalImages;
+
+    init();
 
 
-// function getCurrentSlideNo(wrapperLeftPos){
+    // function getCurrentSlideNo(wrapperLeftPos){
 
-    
-// }
+        
+    // }
 
 
-// ADD EVENT HANDLER TO LEFT AND RIGHT BUTTONS
-leftButton.onclick = function(e) {
+    // ADD EVENT HANDLER TO LEFT AND RIGHT BUTTONS
+    leftButton.onclick = function(e) {
 
-    if (wrapperLeft <= MAX_LEFT){
-        wrapperLeft = 0;
-    }
-    else{
-
-        wrapperLeft = wrapperLeft - imageWidth;
-    }
-    
-
-    var id = setInterval(frame, 1);
-    
-    function frame(){
-    
-        if(parseInt(carouselImageWrapperStyle.left) <= wrapperLeft){
-            carouselImageWrapper.style.left = wrapperLeft + 'px';
-            clearInterval(id);
+        if (wrapperLeft <= MAX_LEFT){
+            wrapperLeft = 0;
         }
         else{
-               
-            carouselImageWrapper.style.left = parseInt(carouselImageWrapperStyle.left) - 10 + 'px';
-            
+
+            wrapperLeft = wrapperLeft - imageWidth;
         }
+        
+
+        var id = setInterval(frame, 1);
+        
+        function frame(){
+        
+            if(parseInt(carouselImageWrapperStyle.left) <= wrapperLeft){
+                carouselImageWrapper.style.left = wrapperLeft + 'px';
+                clearInterval(id);
+            }
+            else{
+                
+                carouselImageWrapper.style.left = parseInt(carouselImageWrapperStyle.left) - 10 + 'px';
+                
+            }
+        }
+
     }
 
-}
+    rightButton.onclick = function(e) {
 
-rightButton.onclick = function(e) {
+        if (wrapperLeft >= 0)
+        {
+            wrapperLeft = MAX_LEFT;
 
-    if (wrapperLeft >= 0)
-    {
-        wrapperLeft = MAX_LEFT;
-
-    }
-    else{
-        wrapperLeft = wrapperLeft + imageWidth;
-    }
-
-    var id = setInterval(frame, 1);
-    
-    function frame(){
-    
-        if(parseInt(carouselImageWrapperStyle.left) >= wrapperLeft){
-            carouselImageWrapper.style.left = wrapperLeft + 'px';
-            clearInterval(id);
         }
         else{
-               
-            carouselImageWrapper.style.left = parseInt(carouselImageWrapperStyle.left) + 10 + 'px';
-            
+            wrapperLeft = wrapperLeft + imageWidth;
+        }
+
+        var id = setInterval(frame, 1);
+        
+        function frame(){
+        
+            if(parseInt(carouselImageWrapperStyle.left) >= wrapperLeft){
+                carouselImageWrapper.style.left = wrapperLeft + 'px';
+                clearInterval(id);
+            }
+            else{
+                
+                carouselImageWrapper.style.left = parseInt(carouselImageWrapperStyle.left) + 10 + 'px';
+                
+            }
         }
     }
-}
 
+    indicatorDiv.onclick = xyz;
 
+    function xyz(e){
+        
+        indicators = indicatorDiv.children
+        for(var i=0; i< totalImages; i++){
+            var indicator = indicators[i];
+            
+            if (indicator == e.target){
+                console.log(i);
+            }
+        }
 
-function init(){
-    // GET CONTAINER WIDTH AND HEIGHT
-    carouselContainer = document.getElementsByClassName('carousel-container')[0];
-    carouselContainerStyle = getComputedStyle(carouselContainer);
-
-    carouselContainerWidth = carouselContainerStyle.width;
-    carouselContainerHeight = carouselContainerStyle.height;
-
-    imageWidth = parseInt(carouselContainerWidth);
-
-
-    // SET SLIDER WIDTH, HEIGHT AND Z-INDEX
-    carouselImageWrapper = document.getElementsByClassName('carousel-image-wrapper')[0];
-    carouselImageWrapperStyle= getComputedStyle(carouselImageWrapper);
-
-    carouselImagesList = document.querySelectorAll('.carousel-image-wrapper img');
-
-    totalSliderWidth = parseInt(carouselContainerWidth) * carouselImagesList.length;
-
-    carouselImageWrapper.style.height = carouselContainerHeight;
-    carouselImageWrapper.style.width = totalSliderWidth + 'px';
-    carouselImageWrapper.style.zIndex = '10';
-
-
-    // GET SLIDER LEFT POSITION
-    wrapperLeft = parseInt(carouselImageWrapperStyle.left);
-
-    // ADD BOTTOM INDICATORS
-    addIndicator(carouselImagesList.length);
-
-
-    // ADD LEFT AND RIGHT SLIDER BUTTONS
-    leftButton = document.createElement('button');
-    leftButton.style.width = '40px';
-    leftButton.style.height = '40px';
-    leftButton.style.borderRadius = '50%';
-    leftButton.style.backgroundColor = 'lightgray';
-    leftButton.style.zIndex = '20';
-    leftButton.style.position = 'absolute';
-    leftButton.style.left = '2px';
-    leftButton.style.border = 'none';
-    leftButton.style.top = (parseInt(carouselContainerHeight) / 2 -
-                            parseInt(leftButton.style.height)) + 'px';
-
-    var leftButtonPara = document.createElement('para');
-    leftButtonPara.innerHTML = '&#8249;';
-    leftButton.appendChild(leftButtonPara);
-    carouselContainer.appendChild(leftButton);
-
-
-    rightButton = document.createElement('button');
-    rightButton.style.width = '40px';
-    rightButton.style.height = '40px';
-    rightButton.style.borderRadius = '50%';
-    rightButton.style.backgroundColor = 'lightgray';
-    rightButton.style.zIndex = '20';
-    rightButton.style.position = 'absolute'
-    rightButton.style.right = '2px'
-    rightButton.style.border = 'none';
-    rightButton.style.top = (parseInt(carouselContainerHeight) / 2 -
-                            parseInt(leftButton.style.height)) + 'px';
-
-    var rightButtonPara = document.createElement('para');
-    rightButtonPara.innerHTML = '&#8250;';
-    rightButton.appendChild(rightButtonPara);
-    carouselContainer.appendChild(rightButton);
-
-    // CALCULTE MAXIMUM LEFT SLIDER POSITION
-    MAX_LEFT = - (parseInt(carouselImagesList.length) - 1) * parseInt(carouselContainerWidth);
-}
-
-function addIndicator(imageLength){
-
-    var indicatorDiv = document.createElement('div');
-
-    indicatorDiv.style.position = 'absolute';
-    indicatorDiv.style.bottom = '5px';
-    indicatorDiv.style.zIndex = '20';
-    indicatorDiv.style.width = '100%';
-    indicatorDiv.style.textAlign = 'center';
-
-    activeIndicator = getIndividualIndicator();
-    activeIndicator.style.backgroundColor = 'rgba(255,255,255,1)';
-    activeIndicator.style.width = '18px';
-    activeIndicator.style.height = '18px';
-    
-    indicatorDiv.appendChild(activeIndicator);
-
-    for(var i = 1; i<imageLength; i++){
-        indicatorDiv.appendChild(getIndividualIndicator());
     }
-    carouselContainer.appendChild(indicatorDiv);
+
+    function init(){
+        // GET CONTAINER WIDTH AND HEIGHT
+        carouselContainer = document.getElementsByClassName('carousel-container')[n];
+        carouselContainerStyle = getComputedStyle(carouselContainer);
+
+        carouselContainerWidth = carouselContainerStyle.width;
+        carouselContainerHeight = carouselContainerStyle.height;
+
+        imageWidth = parseInt(carouselContainerWidth);
 
 
+        // SET SLIDER WIDTH, HEIGHT AND Z-INDEX
+        carouselImageWrapper = document.getElementsByClassName('carousel-image-wrapper')[0];
+        carouselImageWrapperStyle= getComputedStyle(carouselImageWrapper);
 
-}
+        carouselImagesList = document.querySelectorAll('.carousel-image-wrapper img');
 
-function getIndividualIndicator(){
+        totalSliderWidth = parseInt(carouselContainerWidth) * carouselImagesList.length;
 
-    var indicator = document.createElement('button');
-    indicator.style.width = '15px';
-    indicator.style.height = '15px';
-    indicator.style.borderRadius = '50%';
-    indicator.style.backgroundColor = 'rgba(255,255,255,0.5)';
-    indicator.style.border = 'none';
-    indicator.style.marginRight= '10px';
-    return indicator;
+        carouselImageWrapper.style.height = carouselContainerHeight;
+        carouselImageWrapper.style.width = totalSliderWidth + 'px';
+        carouselImageWrapper.style.zIndex = '10';
+
+
+        // GET SLIDER LEFT POSITION
+        wrapperLeft = parseInt(carouselImageWrapperStyle.left);
+
+        // ADD BOTTOM INDICATORS
+        addIndicator(carouselImagesList.length);
+
+
+        // ADD LEFT AND RIGHT SLIDER BUTTONS
+        leftButton = document.createElement('button');
+        leftButton.style.width = '40px';
+        leftButton.style.height = '40px';
+        leftButton.style.borderRadius = '50%';
+        leftButton.style.backgroundColor = 'lightgray';
+        leftButton.style.zIndex = '20';
+        leftButton.style.position = 'absolute';
+        leftButton.style.left = '2px';
+        leftButton.style.border = 'none';
+        leftButton.style.top = (parseInt(carouselContainerHeight) / 2 -
+                                parseInt(leftButton.style.height)) + 'px';
+
+        var leftButtonPara = document.createElement('para');
+        leftButtonPara.innerHTML = '&#8249;';
+        leftButton.appendChild(leftButtonPara);
+        carouselContainer.appendChild(leftButton);
+
+
+        rightButton = document.createElement('button');
+        rightButton.style.width = '40px';
+        rightButton.style.height = '40px';
+        rightButton.style.borderRadius = '50%';
+        rightButton.style.backgroundColor = 'lightgray';
+        rightButton.style.zIndex = '20';
+        rightButton.style.position = 'absolute'
+        rightButton.style.right = '2px'
+        rightButton.style.border = 'none';
+        rightButton.style.top = (parseInt(carouselContainerHeight) / 2 -
+                                parseInt(leftButton.style.height)) + 'px';
+
+        var rightButtonPara = document.createElement('para');
+        rightButtonPara.innerHTML = '&#8250;';
+        rightButton.appendChild(rightButtonPara);
+        carouselContainer.appendChild(rightButton);
+
+        // CALCULTE MAXIMUM LEFT SLIDER POSITION
+        MAX_LEFT = - (parseInt(carouselImagesList.length) - 1) * parseInt(carouselContainerWidth);
+        totalImages = parseInt(carouselImagesList.length);
+    }
+
+    function addIndicator(imageLength){
+
+        indicatorDiv = document.createElement('div');
+
+        indicatorDiv.style.position = 'absolute';
+        indicatorDiv.style.bottom = '5px';
+        indicatorDiv.style.zIndex = '20';
+        indicatorDiv.style.width = '100%';
+        indicatorDiv.style.textAlign = 'center';
+
+        activeIndicator = getIndividualIndicator();
+        activeIndicator.style.backgroundColor = 'rgba(255,255,255,1)';
+        activeIndicator.style.width = '18px';
+        activeIndicator.style.height = '18px';
+        
+        indicatorDiv.appendChild(activeIndicator);
+
+        for(var i = 1; i<imageLength; i++){
+            indicatorDiv.appendChild(getIndividualIndicator());
+        }
+        carouselContainer.appendChild(indicatorDiv);
+
+    }
+
+    function getIndividualIndicator(){
+
+        var indicator = document.createElement('button');
+        indicator.style.width = '15px';
+        indicator.style.height = '15px';
+        indicator.style.borderRadius = '50%';
+        indicator.style.backgroundColor = 'rgba(255,255,255,0.5)';
+        indicator.style.border = 'none';
+        indicator.style.marginRight= '10px';
+        return indicator;
+    }
+
 }
